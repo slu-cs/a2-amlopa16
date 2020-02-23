740d5c1638e2d5cc3c1f92a9bb4de45b199a2087
Amber Lopata <alopata98@gmail.com>
2020-02-23 18:23:43 -0500

    change

--- create.js
+++ create.js
@@ -9,8 +9,9 @@ const rows = [];
 file.on('line', function(line) {
   const columns = line.split(',');
   rows.push({
-    discipline: columns[0],
-    majors: Number(columns[1]),
-    minors: Number(columns[2])
+    first: columns[0],
+    last: columns[1],
+    zip: Number(columns[2]),
+    history: Number(columns[3])
   });
 });
