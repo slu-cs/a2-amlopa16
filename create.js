6427597bb956ed0869563d2a8b43cae23f5fe50c
Amber Lopata <alopata98@gmail.com>
2020-02-23 15:49:31 -0500

    change

--- create.js
+++ create.js
@@ -1,28 +1,8 @@
-const mongodb = require("mongodb");
-const csvtojson = require("csvtojson");
-const Voter = require('./schema');
+var MongoClient = require('mongodb')  ;
+var url = "mongodb://localhost:27017/voters_db";
 
-let url = "mongodb://localhost:27017/";
-
-csvtojson()
-  .fromFile("voters.csv")
-  .then(csvData => {
-    console.log(csvData);
-
-    mongodb.connect(
-      url,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      (err, client) => {
-        if (err) throw err;
-
-        client
-          .db("voters_db")
-          .collection("Voter")
-          .insertMany(csvData, (err, res) => {
-            if (err) throw err;
-
-            client.close();
-          });
-      }
-    );
-  });
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
