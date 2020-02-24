34616021f7fb69d7f125796a4428f354bfd16615
Amber Lopata <alopata98@gmail.com>
2020-02-23 22:24:46 -0500

    change

--- create.js
+++ create.js
@@ -4,13 +4,16 @@ const file = readline.createInterface({
   input: fs.createReadStream('voters.csv')
 });
 
-const rows = [];
+
 file.on('line', function(line) {
   const columns = line.split(',');
-  rows.push({
-    first: columns[0],
-    last: columns[1],
-    zip: Number(columns[2]),
-    history: Number(columns[3])
+  const v = new Voter({
+      first: columns[0],
+      last: columns[1],
+      zip: Number(columns[2]),
+      history: Number(columns[3])
+  });
+  v.save(function(error) {
+    if (error) console.error(error.stack);
   });
 });
