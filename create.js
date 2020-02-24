790fd15ce8c1d459bad71dd90b66b880a2b7b903
Amber Lopata <alopata98@gmail.com>
2020-02-23 22:27:21 -0500

    change

--- create.js
+++ create.js
@@ -16,8 +16,8 @@ file.on('line', function(line) {
   const v = new Voter({
       first: columns[0],
       last: columns[1],
-      zip: Number(columns[2]),
-      history: Number(columns[3])
+      zip: columns[2],
+      history: columns[3]
   });
   v.save(function(error) {
     if (error) console.error(error.stack);
