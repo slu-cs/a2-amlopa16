87adafd1e1d4408a219e71076b3ee3a7f0ca1ef3
Amber Lopata <alopata98@gmail.com>
2020-02-24 15:35:44 -0500

    testing

--- query.js
+++ query.js
@@ -16,19 +16,24 @@ query.exec(function(error, voters) {
 const queries = [
 
   // last name alphabetically
-  Voter.find().sort('-last').limit(1),
+  //Voter.find().sort('-last').limit(1),
 
   // Voters in Canton zip code
-  Voter.find().where('zip').equals('13617'),
+  //Voter.find().where('zip').equals('13617'),
 
-  Voter.find().where('first').equals('STARR')
+  //Voter.find().where('first').equals('STARR'),
+
+  Voter.find().where('history').in('GE16')
+
+
 ];
 
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('Last alphabetical name: ', results[0]);
-    console.log('Canton Zips: ', results[1].map(p => p.zip));
+    //console.log('Last alphabetical name: ', results[0]);
+    //console.log('Canton Zips: ', results[1].map(p => p.zip));
     //console.log('Names with STARR: ', results[2].map(p => p.first));
+    console.log('People who voted in 2016 GE :', results[0]);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
