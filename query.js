19ad37fa0a41aa42d341a645faa4617b70253036
Amber Lopata <alopata98@gmail.com>
2020-02-25 11:13:28 -0500

    finished

--- query.js
+++ query.js
@@ -13,6 +13,9 @@ const queries = [
   // last name alphabetically
   Voter.find().sort('-last').limit(1),
 
+  // how many zips in county
+  Voter.distinct('zip'),
+
   // Voters in Canton zip code
   Voter.countDocuments(Voter.find().where('zip').equals('13617')),
 
@@ -20,18 +23,16 @@ const queries = [
   Voter.find().where('first').equals('STARR'),
 
   // voters in 2016 general election
-  Voter.find({history: /GE16/}),
-
-  // how many zips in county
-  Voter.distinct('zip')
+  Voter.find({history: /GE16/})
 ];
 
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
     console.log('Last alphabetical name: ', results[0].map(p => p.first + ' ' + p.last));
-    console.log('Canton Zips: ', results[1]);
-    console.log('Names with STARR: ', results[2]);
-    console.log('People who voted in 2016 GE :', results[3]);
+    console.log('Number of County Zips', results[1])
+    console.log('Canton Zips: ', results[2]);
+    console.log('Names with STARR: ', results[3]);
+    console.log('People who voted in 2016 GE :', results[4]);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
