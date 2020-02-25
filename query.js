32e15ffecd58b3992bc053ff3f0bc445cec88191
Amber Lopata <alopata98@gmail.com>
2020-02-25 11:00:55 -0500

    test

--- query.js
+++ query.js
@@ -9,15 +9,15 @@ connect(); // To the database
 // What documents are in the collection?
 
 const queries = [
-  //Voter.countDocuments()
 
   // last name alphabetically
-  //Voter.find().sort('-last').limit(1),
+  Voter.find().sort('-last').limit(1),
 
   // Voters in Canton zip code
-  //Voter.find().where('zip').equals('13617'),
+  Voter.countDocuments().find().where('zip').equals('13617'),
 
-  //Voter.find().where('first').equals('STARR')
+  // voters with first name starr
+  Voter.find().where('first').equals('STARR')
 
   Voter.find({history: /GE16/})
 
@@ -27,10 +27,9 @@ const queries = [
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log(results[0]);
-    //console.log('Last alphabetical name: ', results[0].map(p => p.last));
-    //console.log('Canton Zips: ', results[1].map(p => p.last));
-    //console.log('Names with STARR: ', results[2].map(p => p.last));
-    //console.log('People who voted in 2016 GE :', results[3]);
+    console.log('Last alphabetical name: ', results[0].map(p => p.first + p.last));
+    console.log('Canton Zips: ', results[1].map(p => p.last));
+    console.log('Names with STARR: ', results[2]);
+    console.log('People who voted in 2016 GE :', results[3]);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
