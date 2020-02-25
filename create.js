283d02e946ed294142006439a229ea28d178a03e
Amber Lopata <alopata98@gmail.com>
2020-02-25 10:39:39 -0500

    test

--- create.js
+++ create.js
@@ -4,28 +4,30 @@ const Voter = require('./schema');
 
 connect(); // To the database
 
-const fs = require('fs');
-const readline = require('readline');
-const file = readline.createInterface({
-  input: fs.createReadStream('voters.csv')
-});
+mongoose.connection.dropDatabase(function() {
 
-
-const voters = [];
-file.on('line', function(line) {
-  const columns = line.split(',');
-  let v = new Voter({
-      first: columns[0],
-      last: columns[1],
-      zip: columns[2],
-      history: columns[3]
+  const fs = require('fs');
+  const readline = require('readline');
+  const file = readline.createInterface({
+    input: fs.createReadStream('voters.csv')
   });
-  voters.push(v);
-});
 
-file.on('close', function() {
-  mongoose.connection.dropDatabase()
-    .then(() => Promise.all(voters.map(v => v.save())))
-    .then(() => mongoose.connection.close())
-    .catch(error => console.log(error));
+
+  const voters = [];
+  file.on('line', function(line) {
+    const columns = line.split(',');
+    let v = new Voter({
+        first: columns[0],
+        last: columns[1],
+        zip: columns[2],
+        history: columns[3]
+    });
+    voters.push(v.save());
+  });
+
+  file.on('close', function() {
+      Promise.all(voters)
+      .then(() => mongoose.connection.close())
+      .catch(error => console.log(error));
+  });
 });
