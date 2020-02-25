73ae67672a04dc1d2f865215ad178f1fe025924a
Amber Lopata <alopata98@gmail.com>
2020-02-25 08:47:26 -0500

    test

--- create.js
+++ create.js
@@ -11,7 +11,7 @@ const file = readline.createInterface({
 });
 
 
-const saves = [];
+const voters = [];
 file.on('line', function(line) {
   const columns = line.split(',');
   const v = new Voter({
@@ -20,14 +20,13 @@ file.on('line', function(line) {
       zip: columns[2],
       history: columns[3]
   });
-  v.save();
+  voters.push(v);
 });
 
-mongoose.connection.dropDatabase();
-
-file.on('close', function(error) {
-  if (error) console.error(error.stack);
-    //.then(() => Promise.all(saves))
-  mongoose.connection.close();
-  process.exit(0);
+file.on('close', function() {
+  mongoose.connection.dropDatabase()
+    .then(() => Promise.all(saves))
+    .then(() => mongoose.connection.close())
+    .then(() => process.exit(0))
+    .catch(error => console.log(error));
 });
