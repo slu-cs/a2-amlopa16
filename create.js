3127c18c95556b8b31c704672882a79cf0131edf
Amber Lopata <alopata98@gmail.com>
2020-02-25 09:07:28 -0500

    testy

--- create.js
+++ create.js
@@ -27,7 +27,6 @@ file.on('close', function() {
   mongoose.connection.dropDatabase()
     .then(() => Promise.all(voters.map(v => v.save())))
     .then(() => mongoose.connection.close())
+    .then(() => process.exit(0))
     .catch(error => console.log(error));
 });
-
-process.exit(0);
